cc2c13603ed4eaa6e6a93201764b0ccef180f4a1
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2022-08-01 00:50:33 +0300

    finish app and deploy it
    needs to fix bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+      </Stack>
+      <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+    </Box>
   )
 }
 
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -4,6 +4,8 @@ import { Box, Typography } from '@mui/material';
 
 //import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
+import ExerciseCard from './ExerciseCard';
+
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
@@ -27,7 +29,7 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+const HorizontalScrollbar = ({ data, isBodyParts, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
     {data.map((item) => (
       <Box
@@ -36,10 +38,10 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
         title={item.id || item}
         m="0 40px"
       >
-        <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+        {isBodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} />}
       </Box>
     ))}
   </ScrollMenu>
 );
 
-export default HorizontalScrollbar;
+export default HorizontalScrollbar
